b15cfd41f54b34db8cbcb45f19219758b3441ace
techswarn <tech.swarns@gmail.com>
2024-05-17 13:59:26 +0530

    updated CORS

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func NewFiberApp() *fiber.App {
 	// 	},
 	// }))
 	app.Use(cors.New(cors.Config{
-        AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+        AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin,Authorization",
         AllowOrigins:     "https://test1.techenv.dev",
         AllowCredentials: true,
         AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
